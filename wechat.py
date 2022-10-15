443ece777722ae80cea6b3d8943a6392564d4eb6
Westwall98 <35027934+Westwall98@users.noreply.github.com>
2022-10-15 16:10:05 +0800

    Update wechat.py

--- wechat.py
+++ wechat.py
@@ -11,13 +11,15 @@ def get_weixin():
     wechaturl = 'https://mp.weixin.qq.com/cgi-bin/appmsg'
 
     headers = {
-        "Cookie": "appmsglist_action_3546538960=card; ptui_loginuin=591485253; RK=hSVMo6CFEV; ptcz=cf5c8868c68c4fb58ec979c903afaaf40661df73df8628bfd210033f84f4d083; ua_id=h6Iblk27r1q8qmgGAAAAAK9IeT6_-lEylpxO8jxZhxU=; wxuin=65750417591563; mm_lang=zh_CN; skey=@i6qYPoCrE; uin=o591485253; uuid=4b62143d6143aff2e1073f26f3ecea24; rand_info=CAESIPgN6zFWsnBu/B3V9DbmHNGZGwKENpW7rQ4ezNHivDyH; slave_bizuin=3546538960; data_bizuin=3546538960; bizuin=3546538960; data_ticket=WkOR00cxbqxzxFwIlC5Q3EzdIqklLsPJenV/U3h5nOJ+hlPKRgImdpntDEHeCqVw; slave_sid=UDh6amVfNVJVdTdFSUlEWFJKQ0o3eXI5VzFnMDlGODJZbVZudXhrU3FNbzVSb1FPZTNBbGtBaGtfUE1QdFZndGliektFVlVKWTNIVTJLWXV5S0txZTYwR19IN3FkX3BBZFhhS3RINDBzTGpIRkZoTjF5SHJ0ckFCSGl2aHJkWTFsWHhPQ0ZhdVMxMGtHODJN; slave_user=gh_93c255ad7491; xid=6e789a800540b99acefbaff7353d5b13; _clck=3546538960|1|f5p|0; rewardsn=; wxtokenkey=777; wwapp.vid=; wwapp.cst=; wwapp.deviceid=",
+        "Cookie": "ua_id=RITu2IbP6WnFRhvFAAAAAN1Q0z-1CUh9-DpkoMRpiFQ=; wxuin=65816475654888; uuid=93bf6ddc11321744e343eee077050e2e; rand_info=CAESIJggHKgjjgywS6JhOoZp1JvQ0GSm/NgCCU3IrLwU1oMc; slave_bizuin=3546538960; data_bizuin=3546538960; bizuin=3546538960; data_ticket=nhJ3QHXotYjae3mIjmkc+/vofkZGH5zuARAQIgL98i7XRJD7JXpMcu/a4OSln0Bl; slave_sid=ZTlvdUhhbjFzZkUzQURkQXRGRWxoMzVYdmVDMVV0RHZaVFNUWGFON0xrTUJ1VXJBQUc1Z2VDYkE5N1dHZk5lMGpwRjhIaHJkRVRkMG1HSnBoekFDVWducDVrM01QWlYxMUNjVWJoa0tDc0dvaTJ2ZjdTcGFpYWNtVllGTWYzNmhwZ2NsYTQxT2VRbURTaGtq; slave_user=gh_93c255ad7491; xid=b2a158ccc7423e891f3e13e6c0803588; mm_lang=zh_CN; _clck=3546538960|1|f5q|0",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
     }
 
-    token = '844699240'
+    token = '131441509'
 
-    params = {'token': '{}'.format(token), 'lang': 'zh_CN', 'f': 'json', 'ajax': '1', 'action': 'list_ex', 'begin': '0', 'count': '1', 'query': '', 'fakeid': 'MzU3MjYzNjc0Nw==', 'type': '9'}
+    fakeid = 'Mzg3NTQ0MjQwNg=='
+
+    params = {'token': '{}'.format(token), 'lang': 'zh_CN', 'f': 'json', 'ajax': '1', 'action': 'list_ex', 'begin': '1', 'count': '1', 'query': '', 'fakeid': '{}'.format(fakeid), 'type': '9'}
 
     content_json = requests.get(wechaturl, headers=headers, params=params,verify=False).json()
 
@@ -32,9 +34,10 @@ def get_weixin():
 
 def get_news():
 
-    news_html = requests.get(link)
+    news_html = requests.get('https://mp.weixin.qq.com/s?__biz=Mzg3NTQ0MjQwNg==&mid=2247485689&idx=1&sn=039bf94b16362731cc7329947c93c39b&chksm=cec03170f9b7b8666e26b5bf519828c9a3d387fe6e68148a5d2862d093c39df6bbe415cd68d0&token=131441509&lang=zh_CN#rd')
     soup = BeautifulSoup(news_html.text, "lxml")
-    for i in range(5,20):
-        print(soup.select('div section section p span')[i].text)
 
-get_weixin()
+    for i in range(2,19):
+        print(soup.select('body div div div div div div p span')[i].text)
+
+get_news()
